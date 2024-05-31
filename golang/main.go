b0d89ade4e050d7da04cc52f5215f30b0bd99398
c483481 <charleskojansow@gmail.com>
2024-05-31 18:17:28 +0800

    feat(golang): add compare string function

--- golang/main.go
+++ golang/main.go
@@ -7,5 +7,6 @@ import (
 
 func main() {
 	fmt.Printf("result convert from \"42\": %d\n", utils.ConvertToNumber("42"))
-	fmt.Printf("result convert from \"abc\": %d", utils.ConvertToNumber("abc"))
+	fmt.Printf("result convert from \"abc\": %d\n", utils.ConvertToNumber("abc"))
+	fmt.Printf("result compare string: %t\n", utils.CompareString("same string", "same string"))
 }
