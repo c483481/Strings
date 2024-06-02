12353aeea4c94c5b7dd997ef9353fb1f49f427d3
c483481 <charleskojansow@gmail.com>
2024-06-03 00:50:28 +0800

    feat(golang): add check is email function

---
+++ golang/utils/check_string.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"regexp"
+)
+
+var re = regexp.MustCompile(`^[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum|id)\b$`)
+
+func IsEmail(str string) bool {
+	return re.MatchString(str)
+}
