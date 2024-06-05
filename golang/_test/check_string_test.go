f2b5daf179b5dbf65ca1e01ce40d667b1d2863b5
c483481 <charleskojansow@gmail.com>
2024-06-05 21:54:54 +0800

    feat(golang): add simple check rule function

--- golang/_test/check_string_test.go
+++ golang/_test/check_string_test.go
@@ -73,3 +73,81 @@ func BenchmarkCheckIsEmail(b *testing.B) {
 		})
 	}
 }
+
+func TestCheckSimpleRole(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		shouldBe string
+	}{
+		{
+			name:     "_test 1",
+			input:    "U",
+			shouldBe: "users",
+		},
+		{
+			name:     "_test 2",
+			input:    "A",
+			shouldBe: "admin",
+		},
+		{
+			name:     "_test 3",
+			input:    "SA",
+			shouldBe: "super admin",
+		},
+		{
+			name:     "_test 4",
+			input:    "u",
+			shouldBe: "unknown",
+		},
+		{
+			name:     "_test5",
+			input:    "MA",
+			shouldBe: "unknown",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := utils.SimpleCheckRole(test.input)
+			require.Equal(t, test.shouldBe, result)
+		})
+	}
+}
+
+func BenchmarkCheckSimpleRole(b *testing.B) {
+	tests := []struct {
+		name     string
+		input    string
+		shouldBe string
+	}{
+		{
+			name:  "_test 1",
+			input: "U",
+		},
+		{
+			name:  "_test 2",
+			input: "A",
+		},
+		{
+			name:  "_test 3",
+			input: "SA",
+		},
+		{
+			name:  "_test 4",
+			input: "u",
+		},
+		{
+			name:  "_test5",
+			input: "MA",
+		},
+	}
+
+	for _, test := range tests {
+		b.Run(test.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				utils.SimpleCheckRole(test.input)
+			}
+		})
+	}
+}
